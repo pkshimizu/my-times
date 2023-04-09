1d058b76df5c94841392838cdac2d8d7c725463c
shimizu kenji <shimizu.kenji@noncore.net>
2023-04-09 10:45:30 +0900

    fix: gapの指定が外れる現象に対応

--- src/components/atoms/layouts/flex.tsx
+++ src/components/atoms/layouts/flex.tsx
@@ -11,20 +11,24 @@ type FlexProps =  {
 
 export default function Flex({row, column, align, gap = 1, children}: FlexProps) {
   return (
-    <div className={
-      clsx(
-        "flex",
-        `gap-${gap}`,
-        {
-          'flex-row': row,
-          'flex-col': column,
-          'items-start': align === 'start',
-          'items-end': align === 'end',
-          'items-center': align === 'center',
-          'items-baseline': align === 'baseline'
-        }
-      )
-    }>
+    <div
+      className={
+        clsx(
+          "flex",
+          `gap-${gap}`,
+          {
+            'flex-row': row,
+            'flex-col': column,
+            'items-start': align === 'start',
+            'items-end': align === 'end',
+            'items-center': align === 'center',
+            'items-baseline': align === 'baseline'
+          }
+        )}
+      style={{
+        gap: `${gap * 4}px`
+      }}
+    >
       {children}
     </div>
   )
